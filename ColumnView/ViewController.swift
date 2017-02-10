77bb04d3db164c93eb3e9a406020b4611d394059
Lars Stegman <l.s.stegman@student.tudelft.nl>
2017-02-10 16:19:56 +0100

    Initial Commit

---
+++ ColumnView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColumnView
+//
+//  Created by Lars Stegman on 10-02-17.
+//  Copyright © 2017 Stegman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
